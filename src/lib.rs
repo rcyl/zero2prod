26725ede1d0db4798ff155c3efe67256e6f52704
rcyl <raymondchuayitlong@gmail.com>
2022-04-09 21:44:28 +0800

    Chapter 4

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-
-// use actix_web:: {web, App, HttpResponse, HttpServer};
-// use actix_web::dev::Server;
-// use std::net::TcpListener;
+pub mod telemetry;
 
 
 
--- src/startup.rs
+++ src/startup.rs
@@ -1,14 +1,21 @@
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use std::net::TcpListener;
-
 use crate::routes::{health_check, subscribe};
+use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
+    let server = HttpServer::new(move || {
         App::new()
+            // Middlewares are added using the 'wrap' method on 'App'
+            // Instead of Logger::default
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
